606c17302328b7a5f7f82a2404e9a4e9451c23ff
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-02 11:15:20 +0800

    Revert "My 6th submission"
    This reverts commit 2ef8b4dde0acf1de71c2bb7b4dc5ba8cab34a244.

--- samples/basic/do_while.py
+++ samples/basic/do_while.py
@@ -8,17 +8,6 @@ while n <= 100:
     sum = sum + n
     n = n + 9
 print(sum)
-
-while n <= -100:
-    sum = sum - n
-    n = n + 9
-print(sum)
-
-while n <= 100:
-    sum = sum / n
-    n = n + 9
-print(sum)
-
 # 计算1x2x3x...x100:
 acc = 1
 n = 1
