927bdd441da8e0c9e120aef9230b0956b47983dd
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-02 10:50:01 +0800

    My 4th submission

--- samples/basic/do_print.py
+++ samples/basic/do_print.py
@@ -4,4 +4,6 @@
 print('The quick brown fox', 'jumps over', 'the lazy dog')
 print(300)
 print(100 + 200)
-print('100 + 200 =', 100 + 200)
+print('100 + 200 = ', 100 + 200)
+
+# I Jasper finish this,but I didn't have any idea to change it.
