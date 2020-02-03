6a9c38470414f488be31e4b1e3c65ec815dd2941
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-03 20:54:26 +0800

    update dictionary

--- samples/basic/the_list.py
+++ samples/basic/the_list.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-classmates = ['Michael', 'Bob', 'Tracy']
+classmates = ['Michael', 'Bob', 'Tracy', "Jasper"]
+
+d = {
+    "Michael": 100,
+    "Bob": 90,
+    "Tracy": 38,
+    "Jasper": 100,
+}
+
+for x in d:
+    if d[x] <= 40:
+        print(x,"失败")
+    else:
+        print(x,"成功")
+
 print('classmates =', classmates)
 print('len(classmates) =', len(classmates))
 print('classmates[0] =', classmates[0])
 print('classmates[1] =', classmates[1])
 print('classmates[2] =', classmates[2])
+print('classmates[3] =', classmates[3])
 print('classmates[-1] =', classmates[-1])
 classmates.pop()
 print('classmates =', classmates)
