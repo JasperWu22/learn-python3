bd0424bbaf97b734657a2711ea48c6b7b205548e
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-03 20:10:51 +0800

    update dictionary

--- samples/basic/the_dict.py
+++ samples/basic/the_dict.py
@@ -4,9 +4,11 @@
 d = {
     'Michael': 95,
     'Bob': 75,
-    'Tracy': 85
+    'Tracy': 85,
+    'Jasper': 100,
 }
 print('d[\'Michael\'] =', d['Michael'])
 print('d[\'Bob\'] =', d['Bob'])
 print('d[\'Tracy\'] =', d['Tracy'])
+print('d[\'Jasper\'] =', d['Jasper'])
 print('d.get(\'Thomas\', -1) =', d.get('Thomas', -1))
