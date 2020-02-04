a999f68039fdd2a32741531c718207bd0240da63
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-04 08:42:10 +0800

    update dictionary

--- samples/basic/the_tuple.py
+++ samples/basic/the_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-classmates = ('Michael', 'Bob', 'Tracy')
+classmates = ['Michael', 'Bob', 'Tracy']
 print('classmates =', classmates)
 print('len(classmates) =', len(classmates))
 print('classmates[0] =', classmates[0])
@@ -10,4 +10,5 @@ print('classmates[2] =', classmates[2])
 print('classmates[-1] =', classmates[-1])
 
 # cannot modify tuple:
-classmates[0] = 'Adam'
+classmates[0] = "Jasper"
+print(classmates)
