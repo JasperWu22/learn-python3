69dbbf06a3ad7e92fa9a4a576d81013d65a774d2
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-05 21:45:38 +0800

    This is my task2 for John.

--- samples/JasperJob/task2.py
+++ samples/JasperJob/task2.py
@@ -1,4 +1,12 @@
+import random
+w = []
+e = []
 while True:
     name1 = (input("your name:"))
     if name1 == "Q":
         break
+    x = random.randint(1, 100)
+    w.append(name1)
+    e.append(x)
+print(e)
+print(w)
