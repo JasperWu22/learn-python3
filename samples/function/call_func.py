fad00a3ce2d7cacb55f557298179291c86d692a8
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-05 20:09:59 +0800

    update dictionary

--- samples/function/call_func.py
+++ samples/function/call_func.py
@@ -4,7 +4,8 @@
 x = abs(100)
 y = abs(-20)
 print(x, y)
+t = [1, 2, 3]
+print('len(1, 2, 3) =', len(t))
 print('max(1, 2, 3) =', max(1, 2, 3))
 print('min(1, 2, 3) =', min(1, 2, 3))
 print('sum([1, 2, 3]) =', sum([1, 2, 3]))
-# Jasper finish!
