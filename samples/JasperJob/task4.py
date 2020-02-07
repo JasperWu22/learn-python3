d63a31cbfba849d2256b1b34eec777339dd8525f
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-07 20:34:20 +0800

    My homework for John.

--- samples/JasperJob/task4.py
+++ samples/JasperJob/task4.py
@@ -1,6 +1,9 @@
+# Import turtle
 import turtle
 t = turtle.Pen()
+# Asked player to write how many circles.
 draw = int(input("圆圈数:"))
+# Draw the circles.
 for x in range(draw):
     t.circle(100)
     t.left(360/draw)
