280870546dd3038e89a49de94552c9c1d2d7310e
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-09 14:10:27 +0800

    My homework 1 for John.

---
+++ samples/JasperJob/task5.py
@@ -0,0 +1,18 @@
+import random
+a = random.randint(1, 15)
+time = 0
+while True:
+    d = int(input('输入距离:'))
+    time += 1
+    if d == a:
+        print('猜对了')
+        print('你用了', time, "次去击中这个数字", a, "!")
+        break
+    if d > a:
+        print('太大了')
+        print('')
+        time += 1
+    if d < a:
+        print('太小了')
+        print('')
+        time += 1
