92cd371b8da44f1d65bb94d3e1ae9fd3fa714787
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-11 09:54:51 +0800

    My homework 4 for John.

---
+++ samples/JasperJob/task7.py
@@ -0,0 +1,8 @@
+name = input("what is your name?")
+name1 = int(input("How many names do you want?"))
+while name != "":
+    for x in range(name1):
+        print(name, end = " ")
+    print()
+    name = input("Type another name or none thing:")
+print("Thanks for playing the game!")
