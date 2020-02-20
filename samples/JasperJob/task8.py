adb61542b38223978d4efb8e70959cc18967785b
JasperWu22 <60559563+JasperWu22@users.noreply.github.com>
2020-02-20 09:38:19 +0800

    My homework 5  for John.

--- samples/JasperJob/task8.py
+++ samples/JasperJob/task8.py
@@ -1,5 +1,5 @@
 import turtle
-pen = turtle.Pen()
+t = turtle.Pen()
 turtle.bgcolor("black")
 
 colors = ["red", "green", "yellow", "blue", "orange", "purple", "white", "brown", "gray", "pink"]
@@ -7,6 +7,14 @@ family = []
 
 name = turtle.textinput('my family', "enter a name")
 
-while name != " ":
+while name != "":
     family.append(name)
-    name = turtle.textinput()
+    name = turtle.textinput("my family'", "enter a name")
+
+for x in range(100):
+    t.pencolor(colors[x % len(family)])
+    t.penup()
+    t.forward(x * 4)
+    t.pendown()
+    t.write(family[x % len(family)], font = ("Arial", int((x + 4) / 4), "bold"))
+    t.left(360 / len(family) + 2)
